fe99dc94f9446fed1b4e75362ee1590fd36737f7
William Ariza <william.ariza@almundo.com>
2019-05-13 22:42:08 -0500

    chore: doing it live

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
+import * as path from 'path'
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use('/smartcom', express.static(path.join(__dirname, '../../dist')))
+  app.enableCors()
   await app.listen(3000)
 }
 bootstrap()
