850cc943bd6b0b97b3a4753de5e6023cb7713fb7
William Ariza <william.ariza@almundo.com>
2019-05-31 11:57:57 -0500

    feat: setting url static

--- src/server/main.ts
+++ src/server/main.ts
@@ -6,7 +6,8 @@ import * as express from 'express'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true })
   const PORT = process.env.PORT || 3000
-  app.use('/', express.static(path.join(__dirname, '../../dist/venvan-web')))
+  const URL_STATIC = '../../dist/venvan-web'
+  app.use('/', express.static(path.join(__dirname, URL_STATIC)))
   app.enableCors()
   await app.listen(PORT)
 }
