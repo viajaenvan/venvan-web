7026bca8ff204e02e00ee86e00e35e444fedf40b
William Ariza <william.ariza@almundo.com>
2019-05-30 21:40:24 -0500

    feat: update static app-web

--- src/server/main.ts
+++ src/server/main.ts
@@ -6,7 +6,7 @@ import * as express from 'express'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true })
   const PORT = process.env.PORT || 3000
-  app.use('/', express.static(path.join(__dirname, '../../dist')))
+  app.use('/', express.static(path.join(__dirname, '../../dist/venvan-web')))
   app.enableCors()
   await app.listen(PORT)
 }
