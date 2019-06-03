f73e7e131d5c31f4f913dbce8467e20e003b6081
William Ariza <william.ariza@almundo.com>
2019-06-02 23:00:34 -0500

    chore: base route static

--- src/server/main.ts
+++ src/server/main.ts
@@ -6,9 +6,8 @@ import { join } from 'path'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true })
   const PORT = process.env.PORT || 4201
+  app.use('/', express.static(join(__dirname, '../../dist/venvan-web')))
   app.enableCors()
-  app.use('/venvan', express.static(join(__dirname, '../../dist/venvan-web/')))
-
   await app.listen(PORT)
 }
 bootstrap()
