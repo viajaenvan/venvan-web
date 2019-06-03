6fc3f1f0dc251b2b5dd05c45e9ac3fe223378712
William Ariza <william.ariza@almundo.com>
2019-06-02 22:36:39 -0500

    chore: setting statics

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
-import * as path from 'path'
 import * as express from 'express'
+import { join } from 'path'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true })
   const PORT = process.env.PORT || 4201
-  app.use('/', express.static(path.join(__dirname, '../../dist/venvan-web')))
-
   app.enableCors()
+  app.use('/venvan', express.static(join(__dirname, '../../dist/venvan-web/')))
+
   await app.listen(PORT)
 }
 bootstrap()
